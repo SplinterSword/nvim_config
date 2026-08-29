7fcee32f8f5b4aee30810e5423af71c9dcfa54a4
SplinterSword <sparshj2003@gmail.com>
2026-08-29 18:25:54 +0530

    feat: simplied the my gitmarks plugin declaration

--- lua/custom/plugins/gitmarks.lua
+++ lua/custom/plugins/gitmarks.lua
@@ -4,14 +4,4 @@ vim.opt.rtp:prepend(plugin_path)
 
 local gitmarks = require 'gitmarks'
 
-gitmarks.setup {
-  width = 100,
-}
-
-vim.keymap.set('n', '<leader>g', '<Nop>', {
-  desc = '[G]itmarks',
-})
-
-vim.keymap.set('n', '<leader>gh', function() gitmarks.hello() end, {
-  desc = 'Hello from gitmarks',
-})
+gitmarks.setup()
