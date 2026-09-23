ff71ea1c319e2e0aae6be2fcd2ccc13786d465aa
SplinterSword <sparshj2003@gmail.com>
2026-09-23 22:19:16 +0530

    feat: enabled relative numbers

--- init.lua
+++ init.lua
@@ -158,6 +158,10 @@ do
   vim.o.list = true
   vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+  -- hybrid line numbers
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+
   -- Preview substitutions live, as you type!
   vim.o.inccommand = 'split'
 
